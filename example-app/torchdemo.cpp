12877426976510f747d615e5fc2b2483c1fb76b8
zengxiaohui <2362651588@qq.com>
2021-01-09 15:13:10 +0800

    example-app

---
+++ example-app/torchdemo.cpp
@@ -0,0 +1,15 @@
+﻿// torchdemo.cpp: 定义应用程序的入口点。
+//
+
+#include "torchdemo.h"
+#include <torch/torch.h>
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	torch::Tensor tensor = torch::rand({ 2, 3 });
+	std::cout << tensor << std::endl;
+	int y = getchar();
+	return 0;
+}
