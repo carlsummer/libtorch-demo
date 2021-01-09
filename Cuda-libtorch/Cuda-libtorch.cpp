a9d03d35864a2b988e551cd9d667245f0e4c1e24
zengxiaohui <2362651588@qq.com>
2021-01-09 15:22:59 +0800

    d

---
+++ Cuda-libtorch/Cuda-libtorch.cpp
@@ -0,0 +1,12 @@
+﻿// Cuda-libtorch.cpp: 定义应用程序的入口点。
+//
+
+#include "Cuda-libtorch.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
--- example-app/README.md
+++ example-app/README.md
@@ -1,1 +1,2 @@
 > https://pytorch.org/cppdocs/installing.html
+> https://download.pytorch.org/libtorch/cu102/libtorch-win-shared-with-deps-1.7.1.zip
